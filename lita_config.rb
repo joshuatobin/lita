51ce132eb966bc31cecdde698be003b976696871
Joshua Tobin <joshuatobin@gmail.com>
2016-04-12 10:25:53 -0400

    Set rubby

--- lita_config.rb
+++ lita_config.rb
@@ -4,6 +4,6 @@ Lita.configure do |config|
   config.adapters.hipchat.jid      = ENV['HIPCHAT_JID']
   config.adapters.hipchat.password = ENV['HIPCHAT_PASSWORD']
   config.adapters.hipchat.server   = ENV['HIPCHAT_HOST']
-  config.adapters.hipchat.debug    = ENV['HIPCHAT_DEBUG']
+  config.adapters.hipchat.debug    = true
   config.adapters.hipchat.rooms    = ENV['HIPCHAT_ROOMS']
 end
