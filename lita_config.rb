702727155d144f7074768dd8c19272af67ec7901
Joshua Tobin <joshuatobin@gmail.com>
2016-04-12 10:57:38 -0400

    Set port

--- lita_config.rb
+++ lita_config.rb
@@ -1,10 +1,11 @@
 Lita.configure do |config|
-  config.robot.adapter             = :hipchat
-  config.robot.name                = ENV['HIPCHAT_ROBOT_NAME']
-  config.adapters.hipchat.jid      = ENV['HIPCHAT_JID']
-  config.adapters.hipchat.password = ENV['HIPCHAT_PASSWORD']
-  config.adapters.hipchat.server   = ENV['HIPCHAT_HOST']
-  config.adapters.hipchat.debug    = true
-  config.adapters.hipchat.rooms    = [ENV['HIPCHAT_ROOMS']]
+  config.robot.adapter               = :hipchat
+  config.robot.name                  = ENV['HIPCHAT_ROBOT_NAME']
+  config.adapters.hipchat.jid        = ENV['HIPCHAT_JID']
+  config.adapters.hipchat.password   = ENV['HIPCHAT_PASSWORD']
+  config.adapters.hipchat.server     = ENV['HIPCHAT_HOST']
+  config.adapters.hipchat.debug      = true
+  config.adapters.hipchat.rooms      = [ENV['HIPCHAT_ROOMS']]
   config.adapters.hipchat.muc_domain = ENV['HIPCHAT_MUC']
+  config.http.port                   = ENV['PORT']
 end
