c6d74f0afb126531f27d020c76c4e68b14405190
Joshua Tobin <joshuatobin@gmail.com>
2016-04-12 21:15:02 -0400

    cron

--- lita_config.rb
+++ lita_config.rb
@@ -5,7 +5,7 @@ Lita.configure do |config|
   config.adapters.hipchat.password   = ENV['HIPCHAT_PASSWORD']
   config.adapters.hipchat.server     = ENV['HIPCHAT_HOST']
   config.adapters.hipchat.debug      = true
-  config.adapters.hipchat.rooms      = [ENV['HIPCHAT_ROOMS']]
+  config.adapters.hipchat.rooms      = ENV['HIPCHAT_ROOMS'].split(",")
   config.adapters.hipchat.muc_domain = ENV['HIPCHAT_MUC']
   config.http.port                   = ENV['PORT']
 end
