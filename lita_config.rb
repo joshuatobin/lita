2e25b97b1212efd76c1b0887da800a63022d28dc
Joshua Tobin <joshuatobin@gmail.com>
2016-04-13 10:25:20 -0400

    trello

--- lita_config.rb
+++ lita_config.rb
@@ -8,5 +8,9 @@ Lita.configure do |config|
   config.adapters.hipchat.rooms         = ENV['HIPCHAT_ROOMS'].split(",")
   config.adapters.hipchat.muc_domain    = ENV['HIPCHAT_MUC']
   config.http.port                      = ENV['PORT']
-  config.handlers.timezone.default_zone = 'US/Pacific'  
+  config.handlers.timezone.default_zone = 'US/Pacific'
+  # trello
+  config.handlers.trello.public_key     = ENV['TRELLO_PUB_KEY']
+  config.handlers.trello.token          = ENV['TRELLO_TOKEN']
+  config.handlers.trello.board          = ENV['TRELLO_BOARD']
 end
